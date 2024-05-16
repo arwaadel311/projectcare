6f02ebb2b8f718055badfa5ca282ed2603bb6ab5
arwa <arwaadel833@gmail.com>
2024-05-16 18:35:29 +0300

    final jdddddddcBBSXZADDDOOOrdtgdrfgv0rt5er ggszxdjhjkjpppppp

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -13,16 +13,16 @@ export const generalFields = {
     phoneNumber: joi.string().pattern(new RegExp(/^[0-9]{10}$|^\+[0-9]{11}$/)).required(),
     complaint: joi.string().required(),
     id: joi.string().custom(validateObjectId).required(),
-    // file: joi.object({
-    //     size: joi.number().positive().required(),
-    //     path: joi.string().required(),
-    //     filename: joi.string().required(),
-    //     destination: joi.string().required(),
-    //     mimetype: joi.string().required(),
-    //     encoding: joi.string().required(),
-    //     originalname: joi.string().required(),
-    //     fieldname: joi.string().required()
-    // }),
+    file: joi.object({
+        size: joi.number().positive().required(),
+        path: joi.string().required(),
+        filename: joi.string().required(),
+        destination: joi.string().required(),
+        mimetype: joi.string().required(),
+        encoding: joi.string().required(),
+        originalname: joi.string().required(),
+        fieldname: joi.string().required()
+    }),
     headers: joi.string().required()
 }
 
