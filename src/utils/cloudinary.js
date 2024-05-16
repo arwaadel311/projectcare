0ae0e7ff5b886dd5a54f1db86b3bc266cf85d5ac
arwa <arwaadel833@gmail.com>
2024-05-16 03:31:42 +0300

    final jdddddddcBB

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,13 +8,13 @@ import cloudinary from 'cloudinary';
 
 
 cloudinary.v2.config({
-   // cloud_name:process.env.cloud_name,
-    //api_key:process.env.api_key,
- //   api_secret:process.env.api_secret,
+   cloud_name:process.env.cloud_name,
+    api_key:process.env.api_key,
+   api_secret:process.env.api_secret,
  
-cloud_name:"driicz8ge",
-api_key:"572199224579533",
-api_secret:"XgHcDWa_KL4jxR-vNPkT77yjjN4",  
+// cloud_name:"driicz8ge",
+// api_key:"572199224579533",
+// api_secret:"XgHcDWa_KL4jxR-vNPkT77yjjN4",  
  secure: true
 })
 
