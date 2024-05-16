72edfa3743a93facbc37c18a287bbe5bc26b1844
arwa <arwaadel833@gmail.com>
2024-05-16 21:05:58 +0300

    finalsspczxzxzzz

--- DB/model/patient.model.js
+++ DB/model/patient.model.js
@@ -16,8 +16,8 @@ const patientSchema = new Schema({
   EmailPasswordCode:{type:Number,default:null},
   phone_one: {   type: String,required: true,},
   phone_two: {type: String,required: true,},
-  heartRate:{type:Number},
-  motionRate:{type:Number},
+  heartRate:{type:Number,required:true},
+  motionRate:{type:Number,required:true},
  
   //location
   lat:{type:String},
