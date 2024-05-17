ab747e2e5b3fd81e436fbea5041f4202a32f0a51
arwa <arwaadel833@gmail.com>
2024-05-18 00:42:38 +0300

    final rateeeefdsfcse

--- src/modules/rate/controller/rate.js
+++ src/modules/rate/controller/rate.js
@@ -1,6 +1,14 @@
 import { asyncHandler } from "../../../utils/errorHandling.js"
 
 import rateModel from '../../../../DB/model/rate.model.js'
+
+export const Rates = asyncHandler(async (req, res, next) => {
+    const rate = await rateModel.find()
+
+    
+    return res.status(200).json({ message: "Done", rate })
+
+})
 export const createRate = asyncHandler(async (req, res, next) => {
     const { heartRate, motionRate } = req.body
     const rates = await rateModel.create(
--- src/modules/rate/rate.router.js
+++ src/modules/rate/rate.router.js
@@ -6,6 +6,7 @@ import * as rateController from "./controller/rate.js"
 const router = Router()
 
 
+router.get("/",rateController.Rates)
 
 router.post("/",rateController.createRate)
 //router.get("/seizureLocation",seizureController.sendLocation)
