eb8795eb32c42634a2ec1faa91defd5df8d2d739
arwa <arwaadel833@gmail.com>
2024-06-04 01:47:09 +0300

    final sss

--- DB/model/seizure.model.js
+++ DB/model/seizure.model.js
@@ -7,7 +7,7 @@ const seizureSchema = new Schema({
   heartRate: { type: Number },
   motionRate: { type: Number },
   type: { type: String, enum: ['0', '1'], required: true, default: '1' },
-  Time: { type: Date, required: true },
+ // Time: { type: Date, required: true },
   //location
   lat: { type: Number },
   lng: { type: Number },
