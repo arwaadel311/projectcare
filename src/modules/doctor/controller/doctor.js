bbe490af4cc3e627d9595274b7dd16f876f1bbc9
arwa <arwaadel833@gmail.com>
2024-06-11 19:52:25 +0300

    final motion1

--- src/modules/doctor/controller/doctor.js
+++ src/modules/doctor/controller/doctor.js
@@ -329,110 +329,110 @@ export const getDataHeartbeat = asyncHandler(async (req, res, next) => {
         return next(new Error('In-valid account', { cause: 400 }))
     }
    // console.log(doctor);
-   if (OnePatientDocID.heartRate > 130 || OnePatientDocID.heartRate <50 && OnePatientDocID.motionRate>2) {
+//    if (OnePatientDocID.heartRate > 130 || OnePatientDocID.heartRate <50 && OnePatientDocID.motionRate>2) {
 
-   const html = `<!DOCTYPE html>
-   <html>
-   <head>
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></head>
-   <style type="text/css">
-   body{
-       background-color: #88BDBF;margin: 0px;
-   }
-   </style>
-   <body style="margin:0px;"> 
-   <table border="0" width="50%" style="margin:auto;padding:30px;background-color: #F3F3F3;border:1px solid #630E2B;">
-   <tr>
-   <td>
-   <table border="0" width="100%">
-   <tr>
-   <td>
-   <h1>
-   care bracelet  
-   </h1>
-   </td>
-   <td>
+//    const html = `<!DOCTYPE html>
+//    <html>
+//    <head>
+//        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+//    <style type="text/css">
+//    body{
+//        background-color: #88BDBF;margin: 0px;
+//    }
+//    </style>
+//    <body style="margin:0px;"> 
+//    <table border="0" width="50%" style="margin:auto;padding:30px;background-color: #F3F3F3;border:1px solid #630E2B;">
+//    <tr>
+//    <td>
+//    <table border="0" width="100%">
+//    <tr>
+//    <td>
+//    <h1>
+//    care bracelet  
+//    </h1>
+//    </td>
+//    <td>
    
-   </td>
-   </tr>
-   </table>
-   </td>
-   </tr>
-   <tr>
-   <td>
-   <table border="0" cellpadding="0" cellSpacing="0" style="text-align:center;width:100%;background-color: #fff;">
-   <tr>
-   <td style="background-color:#630E2B;height:100px;font-size:50px;color:#fff;">
-   <img width="50px" height="50px" src="https://res.cloudinary.com/ddajommsw/image/upload/v1670703716/Screenshot_1100_yne3vo.png">
-   </td>
-   </tr>
-   <tr>
-   <td>
-   <h1 style="padding-top:25px; color:#630E2B">${OnePatientDocID.firstName} has a seizure</h1>
+//    </td>
+//    </tr>
+//    </table>
+//    </td>
+//    </tr>
+//    <tr>
+//    <td>
+//    <table border="0" cellpadding="0" cellSpacing="0" style="text-align:center;width:100%;background-color: #fff;">
+//    <tr>
+//    <td style="background-color:#630E2B;height:100px;font-size:50px;color:#fff;">
+//    <img width="50px" height="50px" src="https://res.cloudinary.com/ddajommsw/image/upload/v1670703716/Screenshot_1100_yne3vo.png">
+//    </td>
+//    </tr>
+//    <tr>
+//    <td>
+//    <h1 style="padding-top:25px; color:#630E2B">${OnePatientDocID.firstName} has a seizure</h1>
 
 
-   </td>
-   </tr>
-   <tr>
-   <td>
-   <p style="padding:0px 100px;">
-   </p>
-   </td>
-   </tr>
-   <tr>
-   <td>
-   <p style="margin:10px 0px 30px 0px;border-radius:4px;padding:10px 20px;border: 0;color:#fff;background-color:#630E2B; ">
+//    </td>
+//    </tr>
+//    <tr>
+//    <td>
+//    <p style="padding:0px 100px;">
+//    </p>
+//    </td>
+//    </tr>
+//    <tr>
+//    <td>
+//    <p style="margin:10px 0px 30px 0px;border-radius:4px;padding:10px 20px;border: 0;color:#fff;background-color:#630E2B; ">
    
 
    
-</p>
+// </p>
 
-   </td>
-   </tr>
-</td>
-</tr>
-<tr>
-<td>
+//    </td>
+//    </tr>
+// </td>
+// </tr>
+// <tr>
+// <td>
 
-</td>
-</tr>
+// </td>
+// </tr>
        
-   </table>
-   </td>
-   </tr>
-   <tr>
-   <td>
-   <table border="0" width="100%" style="border-radius: 5px;text-align: center;">
-   <tr>
-   <td>
-   <h3 style="margin-top:10px; color:#000">notification doc</h3>
-   </td>
-   </tr>
-   <tr>
-   <td>
-   <div style="margin-top:20px;">
+//    </table>
+//    </td>
+//    </tr>
+//    <tr>
+//    <td>
+//    <table border="0" width="100%" style="border-radius: 5px;text-align: center;">
+//    <tr>
+//    <td>
+//    <h3 style="margin-top:10px; color:#000">notification doc</h3>
+//    </td>
+//    </tr>
+//    <tr>
+//    <td>
+//    <div style="margin-top:20px;">
 
-   </div>
-   </td>
-   </tr>
-   </table>
-   </td>
-   </tr>
-   </table>
-   </body>
-   </html>`
-if (!await sendEmail({ to: doctor.email, subject: 'notificationDoc', html })) {
-   return next(new Error(`fail to send this email`, { cause: 200 }))
-}
+//    </div>
+//    </td>
+//    </tr>
+//    </table>
+//    </td>
+//    </tr>
+//    </table>
+//    </body>
+//    </html>`
+// if (!await sendEmail({ to: doctor.email, subject: 'notificationDoc', html })) {
+//    return next(new Error(`fail to send this email`, { cause: 200 }))
+// }
 
-return res.status(200).json({
-    message: "Done", 
+// return res.status(200).json({
+//     message: "Done", 
     
-    type:"1"
-})
-}
+//     type:"1"
+// })
+// }
     
-return res.status(200).json({ message: "Done" ,type:"0",OnePatientDocID,doctor})
+return res.status(200).json({ message: "Done" ,OnePatientDocID,doctor})
 
 })
 
